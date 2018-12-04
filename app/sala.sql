8da92f217e68a43e883eb48f353c5a6283c6f0f0
Joan0o <shuan779@gmail.com>
2018-12-04 02:18:10 -0500

    y

--- app/sala.sql
+++ app/sala.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-11-2018 a las 08:07:56
+-- Tiempo de generación: 04-12-2018 a las 08:16:11
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.10
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `banda`
 --
 
-create database if not exists sala;
+create database sala;
 use sala;
 
 CREATE TABLE `banda` (
@@ -52,7 +52,23 @@ INSERT INTO `banda` (`id`, `nombre`, `bio`, `estado`, `created_at`, `updated_at`
 (6, 'Banda de customer', 'dubi dubi duba dubi dubi dubaa', -1, '2018-11-23 05:41:00', '2018-10-23 08:50:32'),
 (7, 'hmp', '123', -1, '2018-11-23 05:36:56', '2018-11-23 10:36:56'),
 (8, 'Dmind', 'una banda', 1, '2018-11-23 10:41:20', '2018-11-23 10:41:20'),
-(9, 'Mine', 'famous', 1, '2018-11-23 10:49:05', '2018-11-23 10:49:05');
+(9, 'Mine', 'famous', -1, '2018-12-03 22:47:16', '2018-12-04 03:47:15'),
+(10, 'Banda para mostrarle a mi papi', 'jijoji', -1, '2018-12-03 22:41:52', '2018-12-04 03:41:52'),
+(11, 'Hump', 'qwe', -1, '2018-12-03 23:44:09', '2018-12-04 04:44:09'),
+(12, 'asdlkn', '21qwwr', -1, '2018-12-03 23:24:19', '2018-12-04 04:24:19'),
+(13, 'hmp', 'qowine', -1, '2018-12-03 23:25:25', '2018-12-04 04:25:25'),
+(14, 'hmp', 'qowine', -1, '2018-12-03 23:25:20', '2018-12-04 04:25:20'),
+(15, 'Jasdon', '123', -1, '2018-12-03 23:25:32', '2018-12-04 04:25:32'),
+(16, 'Jasdon', '123', -1, '2018-12-03 23:43:52', '2018-12-04 04:43:52'),
+(17, 'Jasdon', '123', -1, '2018-12-03 23:44:49', '2018-12-04 04:44:49'),
+(18, 'Jasdon', '123', -1, '2018-12-03 23:44:59', '2018-12-04 04:44:59'),
+(19, 'Jasdon', '123', -1, '2018-12-03 23:43:39', '2018-12-04 04:43:39'),
+(20, 'Jasdon', '123', -1, '2018-12-04 00:19:56', '2018-12-04 05:19:56'),
+(21, 'Jasdon', '123', -1, '2018-12-03 23:54:18', '2018-12-04 04:54:18'),
+(22, 'Jasdon', '123', -1, '2018-12-04 00:18:39', '2018-12-04 05:18:39'),
+(23, 'Jasdon', '123', -1, '2018-12-04 00:19:19', '2018-12-04 05:19:19'),
+(24, 'Jasdon', '123', -1, '2018-12-04 00:20:44', '2018-12-04 05:20:44'),
+(25, 'hmp', '23', 1, '2018-12-04 05:20:56', '2018-12-04 05:20:56');
 
 -- --------------------------------------------------------
 
@@ -74,7 +90,21 @@ CREATE TABLE `cancion` (
 --
 
 INSERT INTO `cancion` (`id`, `nombre`, `links`, `banda_id`, `created_at`, `updated_at`) VALUES
-(19, '', 'qweqwe', 7, '2018-11-23 10:42:50', '2018-11-23 10:42:50');
+(19, '', 'qweqwe', 7, '2018-11-23 10:42:50', '2018-11-23 10:42:50'),
+(20, 'Una cancion', 'UN link', 9, '2018-12-03 04:34:39', '2018-12-03 04:34:39'),
+(21, 'asdaasdas', 'asd', 9, '2018-12-03 04:38:00', '2018-12-03 04:38:00'),
+(22, 'asdaasdas', 'asd', 9, '2018-12-03 04:38:10', '2018-12-03 04:38:10'),
+(23, 'asdaasdas', 'asd', 9, '2018-12-03 04:38:14', '2018-12-03 04:38:14'),
+(24, 'asdaasdas', 'asd', 9, '2018-12-03 04:38:19', '2018-12-03 04:38:19'),
+(25, 'hmp', 'hmp', 25, '2018-12-04 10:10:06', '2018-12-04 10:10:06'),
+(26, 'hmp', 'hmp', 25, '2018-12-04 10:10:10', '2018-12-04 10:10:10'),
+(27, 'hmp2', 'hmp', 25, '2018-12-04 10:10:15', '2018-12-04 10:10:15'),
+(28, 'ulala', 'hmp', 25, '2018-12-04 10:10:23', '2018-12-04 10:10:23'),
+(29, 'ulala', 'hmp', 25, '2018-12-04 10:10:29', '2018-12-04 10:10:29'),
+(30, 'bump', '123', 25, '2018-12-04 10:24:26', '2018-12-04 10:24:26'),
+(31, 'bump', '123', 25, '2018-12-04 10:25:17', '2018-12-04 10:25:17'),
+(32, 'bump', '123', 25, '2018-12-04 10:25:56', '2018-12-04 10:25:56'),
+(33, 'blurp', '123', 25, '2018-12-04 10:26:08', '2018-12-04 10:26:08');
 
 -- --------------------------------------------------------
 
@@ -143,32 +173,39 @@ CREATE TABLE `ensayo` (
   `id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `fecha_programada` varchar(15) DEFAULT NULL,
-  `hora` int(11) DEFAULT NULL,
+  `hora` int(11) NOT NULL,
   `duracion` int(11) DEFAULT NULL,
   `estado` text,
   `pagado` decimal(10,0) DEFAULT NULL,
   `banda_id` int(11) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `contacto` varchar(200) DEFAULT NULL,
-  `detalles_admin` text,
-  `detalles_usuario` text
+  `detalles` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `ensayo`
 --
 
-INSERT INTO `ensayo` (`id`, `created_at`, `fecha_programada`, `hora`, `duracion`, `estado`, `pagado`, `banda_id`, `updated_at`, `contacto`, `detalles_admin`, `detalles_usuario`) VALUES
-(8, '2018-11-19 10:40:38', '2018-11-19', 7, 1, 'reservado', '0', 1, '2018-11-19 10:40:38', '342 - admin@admin.com', '', ''),
-(9, '2018-11-23 06:10:00', '2018-11-23', 7, 1, 'reservado', '0', 2, '2018-11-23 06:10:00', NULL, NULL, NULL),
-(10, '2018-11-23 06:14:37', '2018-11-23', 8, 1, 'reservado', '0', 2, '2018-11-23 06:14:37', NULL, NULL, NULL),
-(11, '2018-11-23 06:22:11', '2018-11-23', 9, 1, 'reservado', '0', 2, '2018-11-23 06:22:11', NULL, NULL, NULL),
-(12, '2018-11-23 06:23:47', '2018-11-23', 10, 1, 'reservado', '0', 2, '2018-11-23 06:23:47', NULL, NULL, NULL),
-(13, '2018-11-23 06:28:54', '2018-11-23', 11, 1, 'reservado', '0', 2, '2018-11-23 06:28:54', NULL, NULL, NULL),
-(14, '2018-11-23 07:06:27', '2018-11-23', 12, 1, 'reservado', '0', 2, '2018-11-23 07:06:27', NULL, NULL, NULL),
-(15, '2018-11-23 07:07:34', '2018-11-23', 13, 1, 'reservado', '0', 2, '2018-11-23 07:07:34', NULL, NULL, NULL),
-(16, '2018-11-23 08:44:51', '2018-11-23', 14, 1, 'reservado', '0', 2, '2018-11-23 08:44:51', NULL, NULL, NULL),
-(17, '2018-11-23 11:38:50', '2018-11-23', 15, 1, 'reservado', '0', 9, '2018-11-23 11:38:50', NULL, NULL, NULL);
+INSERT INTO `ensayo` (`id`, `created_at`, `fecha_programada`, `hora`, `duracion`, `estado`, `pagado`, `banda_id`, `updated_at`, `contacto`, `detalles`) VALUES
+(24, '2018-12-03 00:00:14', '2018-12-02', 14, 1, 'cancelado', '0', 1, '2018-12-03 02:49:45', 'Joan Varon - 234234234', 'la sala está dañada'),
+(25, '2018-12-03 00:02:10', '2018-12-02', 15, 1, 'cancelado', '0', 9, '2018-12-03 00:09:59', NULL, NULL),
+(26, '2018-12-03 00:06:58', '2018-12-02', 16, 1, 'cancelado', '0', 9, '2018-12-03 00:27:58', NULL, NULL),
+(27, '2018-12-03 00:32:04', '2018-12-02', 15, 1, 'cancelado', '0', 9, '2018-12-03 00:32:48', NULL, NULL),
+(28, '2018-12-03 00:35:25', '2018-12-02', 15, 1, 'cancelado', '0', 9, '2018-12-03 00:49:11', NULL, NULL),
+(29, '2018-12-03 00:47:27', '2018-12-02', 16, 1, 'cancelado', '0', 9, '2018-12-03 02:44:10', NULL, 'No puedo asistir porque tengo cancer jeje'),
+(30, '2018-12-03 02:30:14', '2018-12-02', 17, 1, 'cancelado', '0', 9, '2018-12-03 03:30:33', NULL, 'error'),
+(31, '2018-12-03 03:28:20', '2018-12-02', 18, 1, 'reservado', '0', 1, '2018-12-03 03:28:20', '342 - jkfsala@gmail.com', NULL),
+(32, '2018-12-03 03:49:27', '2018-12-02', 19, 2, 'reservado', '0', 1, '2018-12-03 03:49:27', '342 - jkfsala@gmail.com', NULL),
+(33, '2018-12-03 03:49:45', '2018-12-02', 0, 2, 'cancelado', '0', 1, '2018-12-03 04:09:57', '342 - jkfsala@gmail.com', '123'),
+(34, '2018-12-03 04:19:04', '2018-12-03', 7, 1, 'confirmado', '1000', 1, '2018-12-04 06:35:36', '342 - jkfsala@gmail.com', 'no pues'),
+(35, '2018-12-03 04:30:57', '2018-12-03', 8, 1, 'confirmado', '2000', 9, '2018-12-04 06:36:05', NULL, 'hmpp'),
+(36, '2018-12-04 04:27:10', '2018-12-03', 19, 1, 'confirmado', '1200', 9, '2018-12-04 10:03:19', NULL, 'emasd'),
+(37, '2018-12-04 04:27:39', '2018-12-03', 20, 1, 'reservado', '0', 9, '2018-12-04 04:27:39', NULL, NULL),
+(38, '2018-12-04 10:49:43', '2018-12-04', 7, 1, 'cancelado', '0', 25, '2018-12-04 10:58:13', NULL, 'No puedo asistir'),
+(39, '2018-12-04 10:54:35', '2018-12-04', 8, 1, 'confirmado', '1200', 25, '2018-12-04 11:16:50', NULL, 'hump'),
+(40, '2018-12-04 10:56:30', '2018-12-04', 9, 1, 'reservado', '0', 25, '2018-12-04 10:56:30', NULL, NULL),
+(41, '2018-12-04 10:57:31', '2018-12-04', 10, 1, 'reservado', '0', 25, '2018-12-04 10:57:31', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -295,10 +332,10 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `email`, `password`, `celular`, `documento`, `foto`, `remember_token`, `email_verified_at`, `updated_at`, `created_at`, `rol`) VALUES
-(2, 'Joan Sebastián Varón forero', 'shuan779@gmail.com', '$2y$10$nC1jZGlAWUFCMYVZDaLbkeF4lvKVlwUktozUKnwnlD9kr.wX3PNUS', '3178230128392', NULL, NULL, 'RXYffDZlpaGn7VmbT4TPCy8hSBLJKUgHy6RUi1hIDN2IrwEL06vo0iOfct4U', '2018-11-23 05:48:43', '2018-11-23 05:48:43', '2018-11-23 05:48:43', ''),
+(2, 'Joan Sebastián Varón forero', 'shuan779@gmail.com', '$2y$10$AAc5.1API0nDKn68EPTgfuiOO5jQbOedkERB6zKOILk0NcdSloRI6', '3178230128392', NULL, NULL, 'ntKBRz0QK0PkOZ48rwqD1EYKxrvgyZcQMDCFZzivvFd5o2DGO3JBAzgOBdaH', '2018-12-04 06:06:55', '2018-12-04 06:06:55', '2018-12-04 06:06:55', ''),
 (3, 'Customer', 'customer@customer.com', '$2y$10$KSrHuIEMDCaDWDJfjxxmVeL1XTjqwn2.gf1/vjets53/oNTpUf2TW', NULL, NULL, NULL, NULL, '2018-10-23 03:47:26', '2018-10-23 08:47:26', '2018-10-23 08:47:26', ''),
 (4, 'custom', 'custom@custom.com', '$2y$10$/CeSnLYfPqanykt4EPg9yuLeW9tPeD8hhNKOtib9.zf2iOx9JmqZC', NULL, NULL, NULL, 'uTjrcShiNvqCyGjb7OxhkU6l4ISbkLAqZqDt12MYppz0pXOqHKzafVqIZMKh', '2018-11-23 05:37:14', '2018-11-23 05:37:14', '2018-11-23 05:37:14', NULL),
-(5, 'admin', 'jkfsala@gmail.com', '$2y$10$7VBbqZHYLE8aKQLe4gEisehBa.uDAkU1u.rOUzJbAqM4CDnmBw5tW', '342', NULL, NULL, NULL, '2018-11-23 02:11:08', '2018-11-23 02:11:08', '2018-11-23 02:11:08', 'admin'),
+(5, 'admin', 'jkfsala@gmail.com', '$2y$10$7VBbqZHYLE8aKQLe4gEisehBa.uDAkU1u.rOUzJbAqM4CDnmBw5tW', '342', NULL, NULL, 'GcoWO3UkZkTp8tp67cIhIQgBTHqn2eiFKekI8FAb800KyA1Ib58WCfdJ3GHd', '2018-12-04 05:09:30', '2018-12-04 05:09:30', '2018-12-04 05:09:30', 'admin'),
 (6, 'a', 'a@a.a', '$2y$10$dQgdw5oPEpBfPmgKhZjeFO3z7dEW6DYbpW6ALA0bFKiisTbreRHD2', NULL, NULL, NULL, 'hmEXld4VSD9eDQbw1KcN7ECOo31aZ8g40mxbiBIJyzYiLGuaRXq1gvklra4c', '2018-11-23 05:44:04', '2018-11-23 05:44:04', '2018-11-23 05:44:04', NULL),
 (7, 'b', 'b@b.b', '123123', '123', '123', '123', NULL, NULL, '2018-11-23 05:46:07', '2018-11-23 05:46:07', NULL),
 (8, 'c', 'c@c.c', '123123', '123', '123', '123', NULL, NULL, '2018-11-23 05:46:32', '2018-11-23 05:46:32', NULL),
@@ -322,7 +359,29 @@ CREATE TABLE `usuario_has_banda` (
 --
 
 INSERT INTO `usuario_has_banda` (`usuario_id`, `banda_id`, `activo`) VALUES
-(2, 9, 1);
+(2, 9, 1),
+(2, 10, 1),
+(2, 11, 1),
+(2, 12, 1),
+(2, 13, 1),
+(2, 14, 1),
+(2, 15, 1),
+(2, 16, 1),
+(2, 17, 1),
+(2, 18, 1),
+(2, 19, 1),
+(2, 20, 1),
+(2, 21, 1),
+(2, 22, 1),
+(2, 23, 1),
+(2, 24, 1),
+(2, 25, 1),
+(3, 25, 1),
+(4, 9, 1),
+(6, 9, 1),
+(7, 25, 1),
+(9, 25, 1),
+(10, 25, 1);
 
 -- --------------------------------------------------------
 
@@ -457,13 +516,13 @@ ALTER TABLE `usuario_has_curso`
 -- AUTO_INCREMENT de la tabla `banda`
 --
 ALTER TABLE `banda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `cancion`
 --
 ALTER TABLE `cancion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
@@ -487,7 +546,7 @@ ALTER TABLE `ejercicio`
 -- AUTO_INCREMENT de la tabla `ensayo`
 --
 ALTER TABLE `ensayo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de la tabla `membresia`
