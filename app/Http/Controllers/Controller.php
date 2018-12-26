1b1fb33cc85edb05a91fd82b52d80f88c721f1d8
Joan0o <30505779+Joan0o@users.noreply.github.com>
2018-12-25 21:55:07 -0500

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function principal(){
+        return view('welcome');
+    }
+    public function bandasusu(){
+        return json_encode(Auth::user()->bandas);
+    }
+    public function cancelar_ensayo($id){
+        return view('modals.razon')->with($id);
+    }
+    
+    
 }
